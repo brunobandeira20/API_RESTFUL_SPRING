ef7ec6497dcf015e6ccf6d422abc79c48e1cfd63
Bruno Bandeira <brunobandeira20@gmail.com>
2022-07-09 20:16:51 -0300

    add camada basica, dominio usuario

---
+++ src/main/java/com/brunobandeira/workshopmongo/resources/UserResouce.java
@@ -0,0 +1,30 @@
+package com.brunobandeira.workshopmongo.resources;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brunobandeira.workshopmongo.domain.User;
+import com.brunobandeira.workshopmongo.dto.UserDTO;
+import com.brunobandeira.workshopmongo.services.UserService;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResouce {
+	
+	@Autowired
+	private UserService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<UserDTO>> findAll(){
+		List<User> list = service.findAll();
+		List<UserDTO> listDto = list.stream().map(obj -> new UserDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+
+}
